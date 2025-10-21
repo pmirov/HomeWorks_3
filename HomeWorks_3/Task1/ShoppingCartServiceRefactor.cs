dbbf071a1ae5e7153b58f05df4759cb29f86aaca
Павел Мировщиков <pmirov@gmail.com>
2025-10-21 09:58:02 +0700

    fix

--- HomeWorks_3/Task1/ShoppingCartServiceRefactor.cs
+++ HomeWorks_3/Task1/ShoppingCartServiceRefactor.cs
@@ -4,9 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeWorks_3.Task1
+namespace Shop
 {
-    internal class ShoppingCartServiceRefactor
+    public class ShoppingCartServiceRefactor
     {
+           
+        public decimal CalculateTotalPriceWithQuantities(string customerType, Dictionary<decimal, int> itemsWithQuantities)
+        {
+
+            decimal baseTotal = itemsWithQuantities.Sum(p => p.Value * p.Key);
+            decimal discount = CalculateDiscount(customerType, baseTotal);
+            decimal finalPrice = CalculateFinalPrice(baseTotal, discount);
+            PrintFinalPrice(baseTotal, discount, finalPrice);
+            return finalPrice;
+
+        }
+
+        private static void PrintFinalPrice(decimal baseTotal, decimal discount, decimal finalPrice)
+        {
+            Console.WriteLine($"Base: {baseTotal}, Discount: {discount}, Final: {finalPrice}");
+        }
+
+        private static decimal CalculateFinalPrice(decimal baseTotal, decimal discount)
+        {
+            return baseTotal - discount;
+        }
+
+        private decimal CalculateDiscount(string customerType, decimal baseTotal)
+        {
+            decimal discount = 0;
+            if (customerType == "Regular")
+            {
+                discount = baseTotal * 0.05m; // 5%
+            }
+            return discount;
+        }
+          
     }
 }
