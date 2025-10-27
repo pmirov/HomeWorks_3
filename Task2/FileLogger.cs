ef89a68fc7a1bd7f348de31b694473f859639196
Павел Мировщиков <pmirov@gmail.com>
2025-10-27 11:19:10 +0700

    fix

---
+++ Task2/FileLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class FileLogger : ILogger
+    {
+        public void WriteLine(string recipient)
+        {
+            File.WriteAllText("log.txt", $"Отправлено уведомление для {recipient}");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            ServiceCollection services = new ServiceCollection();
+
+
+            Console.WriteLine("Выберите способ отправки сообщения (1 - email, 2 - sms:");
+            int number = int.Parse(Console.ReadLine());
+            switch (number)
+            {
+                case 1:  services.AddSingleton<INotificationSender, EmailSender>();
+                    break;
+                case 2: services.AddSingleton<INotificationSender, SmsSender>();
+                    break;
+                default:
+                    {
+                        Console.WriteLine("Нужно ввести 1 или 2");
+                        return;
+                    }
+
+            }
+
+        services.AddSingleton<ILogger,FileLogger>();
+           
+//            
+            services.AddSingleton<NotificationService>();
+            ServiceProvider provider= services.BuildServiceProvider();
+
+            var service = provider.GetRequiredService<NotificationService>();
+
+           
+            service.SendNotification("Ваш заказ готов", "user@example.com");
+        }
+    }
+}
